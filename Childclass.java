3deec0368ecdcdc0292fe49b9e6d9c87e095960f
PRAMITARANE <pramitarane94@gmail.com>
2023-04-21 16:22:35 -0400

    new file added

---
+++ Childclass.java
@@ -0,0 +1,18 @@
+package pramita;
+
+public abstract class Childclass implements parentintrface, Pintrface,Grandintrface{
+	public void showchildcls()
+	{System.out.println("i am child class method"); 
+	
+	}
+
+	public void pinterfcemethod() {
+		System.out.println("i am from another interface");
+	}
+	
+ public void getdetails() {
+	 System.out.println("i am the inherited from parent interface get method ");
+ }
+	
+ 
+	}
